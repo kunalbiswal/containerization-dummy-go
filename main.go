fc9c1c08040fc40a88b5f16fbde904c47190b9e2
Enes <ahmedenesturan@gmail.com>
2022-08-28 07:54:15 +0300

    Update main.go

--- main.go
+++ main.go
@@ -16,12 +16,31 @@ func main() {
 	e.Use(middleware.Recover())
 
 	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "Uzaylılar tarafından kaçırıldım. Evet, tarafından.")
+		if err := c.HTML(http.StatusOK, "Uzaylılar tarafından kaçırıldım. Evet, tarafından."); err != nil {
+			return err
+		}
+		return nil
+	}).Name = "index"
+	e.GET("/about", func(c echo.Context) error {
+		if err := c.HTML(http.StatusOK, "Uzaylılar tarafından kaçırıldım. Evet, tarafından."); err != nil {
+			return err
+		}
+		return nil
 	})
 
 	e.GET("/ping", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
+		if err := c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"}); err != nil {
+			return err
+		}
+		return nil
 	})
+
+	e.GET("/health", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
+	}).Name = "health"
+	e.GET("/health/ping", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
+	}).Name = "health-ping"
 
 	httpPort := os.Getenv("HTTP_PORT")
 	if httpPort == "" {
